f632413d65c9b19289ced8876688400ac6943011
patryk <patryk.gorny@microsoft.wsei.edu.pl>
2024-10-15 15:19:54 +0200

    1

--- Lab ASP 1/obj/Debug/net8.0/Lab ASP 1.AssemblyInfo.cs
+++ Lab ASP 1/obj/Debug/net8.0/Lab ASP 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab ASP 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1da191b8727994f0ce4c982435ee61a153537a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2684a44c7ef8cd924247c0b44e4a58dfc2b3d35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab ASP 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab ASP 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
